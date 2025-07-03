5fd112b479938ff88af5bdf5d2d2b6d4e64dabf4
Ishan Tandukar <077bct019.ishan@sagarmatha.edu.np>
2025-07-03 17:37:59 +0545

    Updated supabase credentials

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -9,3 +9,12 @@ const supabaseKey =
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = import.meta.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = import.meta.env.REACT_APP_SUPABASE_ANON_KEY;
+
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
